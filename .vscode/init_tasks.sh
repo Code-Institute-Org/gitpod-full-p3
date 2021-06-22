32636f868a6c882b793656848ac58f2297deaa72
Matt Rudge <matt.rudge@codeinstitute.net>
2021-06-22 14:54:50 +0100

    Remove run aliases from init_tasks.sh

--- .vscode/init_tasks.sh
+++ .vscode/init_tasks.sh
@@ -11,9 +11,5 @@ sed -i "s/USER_NAME/$GITPOD_GIT_USER_NAME/g" ${GITPOD_REPO_ROOT}/README.md
 echo "Creating .sqliterc file"
 echo ".headers on" > ~/.sqliterc
 echo ".mode column" >> ~/.sqliterc
-echo "Adding run aliases"
-echo 'alias run="python3 $GITPOD_REPO_ROOT/manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
-echo 'alias python=python3' >> ~/.bashrc
-echo 'alias pip=pip3' >> ~/.bashrc
 echo "Your workspace is ready to use. Happy coding!"
 source ~/.bashrc
